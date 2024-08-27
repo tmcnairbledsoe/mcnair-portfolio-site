a13007b514ad0d8b04318d6ef336d6ca3672b0e6
tmcnairbledsoe <tmcnairbledsoe@gmail.com>
2024-08-27 09:27:20 -0400

    spelling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,7 +20,7 @@ const Projects = () => {
       <p style={{ textAlign: "left" }}>
         The first thing I should talk about is this site. It's public on my github and is ued for CI/CD. The site is hosted on Azure
         for free as a static web app. It only took me about 3 hours to create the basic site and host it on Azure. Things have become
-        easier andm ore intuitive than ever. I simply hosted the code from VS Code's GitHub addon to my personal repository. I was 
+        easier and more intuitive than ever. I simply hosted the code from VS Code's GitHub addon to my personal repository. I was 
         then able to connect that repo to the azure static web app for CI/CD. Whenever I make a commit to master, the changes are
         automatically uploaded. Gone are the days of FTP and refreshing the site.
       </p>
