7ce4288e85136030e41322235563c465fbec51f4
tmcnairbledsoe <tmcnairbledsoe@gmail.com>
2024-08-28 11:27:12 -0400

    update wedding link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Resume from "./components/Resume";
 import Projects from "./components/Projects";
 import Interests from "./components/Interests";
 import Home from "./components/Home";
-import WeddingSite from "./components/projects/WeddingSite/WeddingSite";
 
 function App() {
   const [sidebarWidth, setSidebarWidth] = useState("50px");
@@ -13,11 +12,11 @@ function App() {
   const handleSidebarToggle = (width) => {
     setSidebarWidth(width);
   };
-  
+
   return (
     <Router>
       <div style={{ position: "relative", height: "100vh" }}>
-      <PixelDrop onSidebarToggle={handleSidebarToggle} />
+        <PixelDrop onSidebarToggle={handleSidebarToggle} />
         <div
           style={{
             position: "absolute",
@@ -25,7 +24,7 @@ function App() {
             left: window.innerWidth >= 769 ? sidebarWidth : 20,
             width: window.innerWidth >= 769 ? `calc(100% - ${sidebarWidth})` : `calc(100% - 20)`,
             zIndex: 2, // Ensures this content is above the pixel drop animation
-            color: "#bfbfbf", // White text
+            color: "#bfbfbf", 
             padding: "20px",
             transition: "left 0.3s ease, width 0.3s ease",
           }}
@@ -35,7 +34,6 @@ function App() {
             <Route path="/resume" element={<Resume />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/interests" element={<Interests />} />
-            <Route path="/projects/weddingSite" element={<WeddingSite />} />
           </Routes>
         </div>
       </div>
